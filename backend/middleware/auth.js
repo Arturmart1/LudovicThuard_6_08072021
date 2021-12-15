3ad56ad0cda5e7fa7782e546bf2825c15ed1ed76
Ludovic T <thuard.ludovic@gmail.com>
2021-12-15 08:35:00 +0100

    Auth failure correction

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,32 +4,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, '${process.env.TOKEN}');
-    const userId = decodedToken.userId;
-    req.auth = { userId };
-    if (req.body.userId && req.body.userId !== userId) { //faille ici
-      throw 'Invalid user ID';
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
-  }
-};
-
-/*
-const jwt = require('jsonwebtoken');
-
-
-// changer la clé random token qui est trop utilisée et facilement trouvable
-module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        req.token = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        req.token = jwt.verify(token, '${process.env.TOKEN}');
         next()
     } catch (error) {
         res.status(401).json({
@@ -38,5 +15,3 @@ module.exports = (req, res, next) => {
     }
 
 };
-
- */
