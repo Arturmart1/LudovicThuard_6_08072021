bab777728cbec768d30bab5a6b027d7d1db8dd76
Ludovic T <thuard.ludovic@gmail.com>
2021-12-15 09:07:39 +0100

    Rework likes & dislikes route

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -8,8 +8,8 @@ const saucesSchema = mongoose.Schema({
     mainPepper: { type: String, required: true },
     imageUrl: { type: String, required: true },
     heat: { type: Number, required: true },
-    likes: { type: Number, default: 0, required: true },
-    dislikes: { type: Number, default: 0, required: true },
+    likes: { type: Number},
+    dislikes: { type: Number},
     usersLiked: { type: [String]},
     usersDisliked: { type: [String]},
 });
