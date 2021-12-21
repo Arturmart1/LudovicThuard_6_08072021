ad63feb649c7b6b6c086e3c858c7e6f9b62ea34b
Ludovic T <thuard.ludovic@gmail.com>
2021-12-21 09:00:29 +0100

    Add express rate limit prevent #1 OWASP vulnerabilities

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,11 @@ const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 const path = require('path');
 
+//Connection à la base de données
+
 const mongoConnect = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.qwua6.mongodb.net/${process.env.DB_NAME}?${process.env.DB_SET}`
+
+//Paramètres de connexion à la base de données
 
 mongoose.connect(mongoConnect, {
   useNewUrlParser: true,
@@ -15,6 +19,8 @@ mongoose.connect(mongoConnect, {
 
 const app = express();
 
+//CORS
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,11 +28,19 @@ app.use((req, res, next) => {
     next();
   });
 
+//Utilitaires
+
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
+
+//import express-rate-limit middleware
+app.use(require('express-rate-limit')({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+}));
 
 module.exports = app;
